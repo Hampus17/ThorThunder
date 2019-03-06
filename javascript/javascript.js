2f9d76e24691e247f41064c494575f3dc75214ac
Hampus Andersson <hampus.a02@gmail.com>
2019-03-06 12:44:44 +0100

    Some more whole site revamp

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,2 +1,12 @@
 // TODO: When scrolling down make the navbar background blurry or a dark shade of grey
-// TODO: 
+// TODO:
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(100)
+    .next()
+    .fadeIn(100)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
