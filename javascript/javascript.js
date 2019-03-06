50af25232c4b0dab8c68c627facd9826c7604f5c
Hampus Andersson <hampus.a02@gmail.com>
2019-03-06 13:23:30 +0100

    Position fixed on nav

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,12 +1,11 @@
 // TODO: When scrolling down make the navbar background blurry or a dark shade of grey
 // TODO:
-$("#slideshow > div:gt(0)").hide();
-
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(100)
-    .next()
-    .fadeIn(100)
-    .end()
-    .appendTo('#slideshow');
-},  3000);
+console.log("test")
+$(window).on('scroll', function () {
+    if ($(window).scrollTop()) {
+        $('nav').addClass('blur');
+    }
+    else {
+        $('nav').removeClass('blur');
+    }
+})
